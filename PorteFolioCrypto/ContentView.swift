60b8952f19d362ad1b4c9c5e8ddfcf7e2c6abad8
Etalzon <Etalzon@gmail.com>
2025-12-07 14:47:07 +0100

    Initial Commit

---
+++ PorteFolioCrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PorteFolioCrypto
+//
+//  Created by eric locci on 07/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PorteFolioCrypto/PorteFolioCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PorteFolioCryptoApp.swift
+//  PorteFolioCrypto
+//
+//  Created by eric locci on 07/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct PorteFolioCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
